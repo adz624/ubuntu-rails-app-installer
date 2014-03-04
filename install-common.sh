8194ef7a4020dddd8892e847d500b560f51598dd
eddie <eddie@visionbundles.com>
2014-03-05 01:05:43 +0800

    Add comment for steps

--- install-common.sh
+++ install-common.sh
@@ -3,20 +3,22 @@
 # @updated_at 2014/3/5
 # @version 0.01
 
+# 1. 更新 package list
 echo -e "\e[31mAptitude - Update & Upgrade\e[0m"
-aptitude update
-aptitude upgrade
+aptitude update && aptitude upgrade
+
+# 2. 安裝常用工具
 echo -e "\e[31mInstall common tools\e[0m"
-# 安裝常用工具
 aptitude install curl git-core python-software-properties bash-completion htop iftop tmux vim -y
 echo -e "\e[31mDownload the shellscript configuration in current user home dir: ~/$(whoami)\e[0m"
 cd ~
 
+# 3. 跳過 public key 檢查
 echo -e "\e[31mSkipping SSH HostKeyChecking ... \e[0m"
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" > ~/.ssh/config
 
+# 4. 下載、並設定 bash, vim, gitconfig
 git clone https://github.com/afunction/dotfiles .dotfiles
-# 設定 bash, vim, gitconfig
 echo -e "\e[31mSetting up bash shell ... \e[0m"
 rm -rf .bash_profile
 ln -s .dotfiles/bash_profile_ubuntu .bash_profile
@@ -29,14 +31,15 @@ echo -e "\e[31mSetting up git ... \e[0m"
 rm -rf .gitconfig
 ln -s .dotfiles/gitconfig .gitconfig
 
+# 5. 更新 vim plugin & bundles
 echo -e "\e[31mDownload vim color schema & bundles ... \e[0m"
 cd .dotfiles
-# 更新 vim, bundles
 git submodule init
 git submodule update
 
-# reload bash setting
+# 6. Reload bash setting
 source ~/.bash_profile
 
+# 7. Download installer for current user
 echo -e "\e[31mDownload Installer shell script ... \e[0m"
 git clone https://github.com/afunction/ubuntu-rails-app-installer.git installer
