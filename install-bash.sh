c9389a10588572bf502b942e06b2e6d051c00a27
eddie <eddie@visionbundles.com>
2014-03-05 01:29:51 +0800

    Add home dir prefix

--- install-bash.sh
+++ install-bash.sh
@@ -5,24 +5,24 @@
 
 # 1. 下載、並設定 bash, vim, gitconfig
 cd ~
-rm -rf .dotfiles
+rm -rf ~/.dotfiles
 git clone https://github.com/afunction/dotfiles .dotfiles
 echo -e "\e[31mSetting up bash shell ... \e[0m"
 rm -rf .bash_profile
 ln -s .dotfiles/bash_profile_ubuntu .bash_profile
 
 echo -e "\e[31mSetting up vim ... \e[0m"
-rm -rf .vimrc
-ln -s .dotfiles/vimrc .vimrc
+rm -rf ~/.vimrc
+ln -s ~/.dotfiles/vimrc ~/.vimrc
 
 echo -e "\e[31mSetting up git ... \e[0m"
-rm -rf .gitconfig
-ln -s .dotfiles/gitconfig .gitconfig
-ln -s .dotfiles/banner .banner
+rm -rf ~/.gitconfig
+ln -s ~/.dotfiles/gitconfig .gitconfig
+ln -s ~/.dotfiles/banner ~/.banner
 
 # 2. 更新 vim plugin & bundles
 echo -e "\e[31mDownload vim color schema & bundles ... \e[0m"
-cd .dotfiles
+cd ~/.dotfiles
 git submodule init
 git submodule update
 
