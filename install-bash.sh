684f182e26456e9aa92654fe0fa356dd6ff655ff
eddie <eddie@visionbundles.com>
2014-03-05 01:49:32 +0800

    Change path to .vim for vim color schema

--- install-bash.sh
+++ install-bash.sh
@@ -4,24 +4,24 @@
 # @version 0.01
 
 # 1. 下載、並設定 bash, vim, gitconfig
-rm -rf ~/.dotfiles
-git clone https://github.com/afunction/dotfiles ~/.dotfiles
+rm -rf ~/.vim
+git clone https://github.com/afunction/dotfiles ~/.vim
 echo -e "\e[31mSetting up bash shell ... \e[0m"
 rm -rf ~/.bash_profile
-ln -s ~/.dotfiles/bash_profile_ubuntu ~/.bash_profile
+ln -s ~/.vim/bash_profile_ubuntu ~/.bash_profile
 
 echo -e "\e[31mSetting up vim ... \e[0m"
 rm -rf ~/.vimrc
-ln -s ~/.dotfiles/vimrc ~/.vimrc
+ln -s ~/.vim/vimrc ~/.vimrc
 
 echo -e "\e[31mSetting up git ... \e[0m"
 rm -rf ~/.gitconfig
-ln -s ~/.dotfiles/gitconfig .gitconfig
-ln -s ~/.dotfiles/banner ~/.banner
+ln -s ~/.vim/gitconfig .gitconfig
+ln -s ~/.vim/banner ~/.banner
 
 # 2. 更新 vim plugin & bundles
 echo -e "\e[31mDownload vim color schema & bundles ... \e[0m"
-cd ~/.dotfiles
+cd ~/.vim
 git submodule init
 git submodule update
 
