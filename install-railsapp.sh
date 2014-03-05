2789147a651ae923e4f70021086393ea89a32dbe
eddie <eddie@visionbundles.com>
2014-03-05 17:05:06 +0800

    Add rvm, ruby, bundler to installer

--- install-railsapp.sh
+++ install-railsapp.sh
@@ -5,9 +5,10 @@
 DEPLOY_USER='deploy'
 NGINX_USER='nginx'
 WEB_GROUP='www-data'
+RUBY_VERSION='2.1.0'
 
 # 1. 安裝 node.js for assets pipeline
-echo -e "\e[31m[RailsAPP] Install runtime javascript library for assets pipelie ...\e[0m"
+echo -e "\e[31m[RailsAPP] Installing runtime js library for assets pipelie ...\e[0m"
 echo -e "\e[31m[RailsAPP] Add node.js source for apt ...\e[0m"
 add-apt-repository ppa:chris-lea/node.js -y
 echo -e "\e[31m[RailsAPP] Update apt package list and install node.js ...\e[0m"
@@ -30,3 +31,19 @@ mkdir /home/$DEPLOY_USER/.install
 cp install-bash.sh /home/$DEPLOY_USER/.install
 chown -R $DEPLOY_USER:$DEPLOY_USER /home/$DEPLOY_USER/.install
 su -c "cd /home/$DEPLOY_USER/.install && ./install-bash.sh" $DEPLOY_USER
+
+# 4. 安裝 rvm stable
+echo -e "\e[31m[RailsAPP] Installing RVM ...\e[0m"
+su -l -c "\curl -sSL https://get.rvm.io | bash -s stable" $DEPLOY_USER
+
+# 5. 更新 RVM
+echo -e "\e[31m[RailsAPP] Update RVM ...\e[0m"
+su -l -c "rvm get head" $DEPLOY_USER
+
+# 6. 安裝 Ruby + Bundler
+echo -e "\e[31m[RailsAPP] Installing Ruby $RUBY_VERSION ...\e[0m"
+su -l -c "rvm install $RUBY_VERSION" $DEPLOY_USER
+su -l -c "rvm --default use $RUBY_VERSION" $DEPLOY_USER
+
+echo -e "\e[31m[RailsAPP] Installing Bundler ...\e[0m"
+su -l -c "gem install bundler" $DEPLOY_USER
