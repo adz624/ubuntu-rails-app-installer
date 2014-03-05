a62a85d2a05daf2b76e1bc4694a99b6705b3620d
root <root@ubuntu.(none)>
2014-03-05 13:58:29 +0800

    Fix percona installer bug

--- install-percona.sh
+++ install-percona.sh
@@ -9,12 +9,12 @@ apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
 
 # 2. Add apt source
 IF_SOURCE_ADDED=$(cat /etc/apt/sources.list | grep repo.percona.com | wc -l)
-if ["$IF_SOURCE_ADDED" == 0]; then
+if [ $IF_SOURCE_ADDED == 0 ]; then
   UBUNTU_DISTRIB=$(cat /etc/*-release | grep DISTRIB_CODENAME | awk 'BEGIN {FS="="}{print $2}')
   echo -e "\e[31m[Percona] Add percona mysql apt source.list ...\e[0m"
   echo -e "\e[31m[Percona] DISTRIB_CODENAME=$UBUNTU_DISTRIB ...\e[0m"
-  echo "deb http://repo.percona.com/apt $UBUNTU_DISTRIB main"
-  echo "deb-src http://repo.percona.com/apt $UBUNTU_DISTRIB main"
+  echo "deb http://repo.percona.com/apt $UBUNTU_DISTRIB main" >> /etc/apt/sources.list
+  echo "deb-src http://repo.percona.com/apt $UBUNTU_DISTRIB main" >> /etc/apt/sources.list
 
   echo -e "\e[31m[Percona] Update apt list ...\e[0m"
   aptitude update
