ab082cdf94619cc904cb3cfbd51856549e400ffe
eddie <eddie@visionbundles.com>
2014-03-05 15:07:44 +0800

    Fix percona installer

--- install-percona.sh
+++ install-percona.sh
@@ -3,13 +3,13 @@
 # @updated_at 2014/3/5
 # @version 0.01
 
-# 1. Add percona mysql apt key
-echo -e "\e[31m[Percona] Add percona mysql apt key ...\e[0m"
-apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
-
-# 2. Add apt source
+# 1. Add apt source
 IF_SOURCE_ADDED=$(cat /etc/apt/sources.list | grep repo.percona.com | wc -l)
 if [ $IF_SOURCE_ADDED == 0 ]; then
+  # 2. Add percona mysql apt key
+  echo -e "\e[31m[Percona] Add percona mysql apt key ...\e[0m"
+  apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
+
   UBUNTU_DISTRIB=$(cat /etc/*-release | grep DISTRIB_CODENAME | awk 'BEGIN {FS="="}{print $2}')
   echo -e "\e[31m[Percona] Add percona mysql apt source.list ...\e[0m"
   echo -e "\e[31m[Percona] DISTRIB_CODENAME=$UBUNTU_DISTRIB ...\e[0m"
@@ -28,6 +28,7 @@ else
   # 3. Setting mysql configuration
   echo -e "\e[31m[Percona] Install mysql ...\e[0m"
   aptitude install percona-server-server-5.5 percona-server-client-5.5 -y
+  /etc/init.d/mysql stop
 
   # 4. Setting mysql configuration
   echo -e "\e[31m[Percona] Setting mysql configuration ...\e[0m"
