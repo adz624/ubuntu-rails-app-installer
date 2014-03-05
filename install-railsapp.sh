48f065f2b5a3d3e4da74b164cd4f8af1963d40f4
eddie <eddie@visionbundles.com>
2014-03-05 16:01:06 +0800

    Add output message

--- install-railsapp.sh
+++ install-railsapp.sh
@@ -7,16 +7,23 @@ NGINX_USER='nginx'
 WEB_GROUP='www-data'
 
 # 1. 安裝 node.js for assets pipeline
+echo -e "\e[31m[RailsAPP] Install runtime javascript library for assets pipelie ...\e[0m"
 add-apt-repository ppa:chris-lea/node.js
 aptitude update
 aptitude install nodejs -y
 
-# 2. 新增 rails application deploy user、加入 sudoer
-#    要把 deploy user 加到 web group，因為 static file 也需要 nginx 也需要有權限存取
+# 2. 新增 rails application deploy user
 useradd $DEPLOY_USER -G $WEB_GROUP -d /home/$DEPLOY_USER
+echo -e "\e[31m[RailsAPP] Setting Deploy user password ...\e[0m"
+passwd $DEPLOY_USER
+
+
+# 3. 加入 sudoer 要把 deploy user 加到 web group，因為 static file 也需要 nginx 也需要有權限存取
+echo -e "\e[31m[RailsAPP] Add deploy user to sudoer list ...\e[0m"
 echo "$DEPLOY_USER  ALL=(ALL:ALL) ALL" >> /etc/sudoers
 
 # 3. 安裝 deploy user bash
+echo -e "\e[31m[RailsAPP] Setting deploy user bash config ...\e[0m"
 mkdir ~$DEPLOY_USER/.install
 cp install-bash.sh ~$DEPLOY_USER/.install
 chown -R $DEPLOY_USER:$DEPLOY_USER ~$DEPLOY_USER/.install
