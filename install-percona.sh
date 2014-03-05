8d2acddea831400a21fc9d4d6200ac4a3ddaee20
eddie <eddie@visionbundles.com>
2014-03-05 13:33:00 +0800

    Fix var setting

--- install-percona.sh
+++ install-percona.sh
@@ -8,9 +8,9 @@ echo -e "\e[31m[Percona] Add percona mysql apt key ...\e[0m"
 apt-key adv --keyserver keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
 
 # 2. Add apt source
-IF_SOURCE_ADDED=cat /etc/apt/sources.list | grep repo.percona.com | wc -l
+IF_SOURCE_ADDED=$(cat /etc/apt/sources.list | grep repo.percona.com | wc -l)
 if [$IF_SOURCE_ADDED == 0]; then
-  UBUNTU_DISTRIB=cat /etc/*-release | grep DISTRIB_CODENAME | awk 'BEGIN {FS="="}{print $2}'
+  UBUNTU_DISTRIB=$(cat /etc/*-release | grep DISTRIB_CODENAME | awk 'BEGIN {FS="="}{print $2}')
   echo -e "\e[31m[Percona] Add percona mysql apt source.list ...\e[0m"
   echo -e "\e[31m[Percona] DISTRIB_CODENAME=$UBUNTU_DISTRIB ...\e[0m"
   echo "deb http://repo.percona.com/apt $UBUNTU_DISTRIB main"
@@ -18,6 +18,8 @@ if [$IF_SOURCE_ADDED == 0]; then
 
   echo -e "\e[31m[Percona] Update apt list ...\e[0m"
   aptitude update
+else
+  echo -e "\e[31m[Percona] source exists ...\e[0m"
 fi
 
 echo -e "\e[31m[Percona] Install mysql ...\e[0m"
