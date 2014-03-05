3aa5a5d80c9e895fc3fd7160d03529a22a0216ca
eddie <eddie@visionbundles.com>
2014-03-05 12:58:51 +0800

    Add nginx installer

--- install-nginx.sh
+++ install-nginx.sh
@@ -1,9 +1,14 @@
-
 #!/bin/bash
 # @author eddie li (ADZ)
 # @updated_at 2014/3/5
 # @version 0.01
 
-# 1. add newest nginx repo
+# 1. add newest nginx repo, update, and install
+echo -e "\e[31m[Nginx] Add newest nginx repo, update, install ...\e[0m"
 add-apt-repository ppa:nginx/stable
 aptitude update
+aptitude install nginx -y
+
+# 2. restart nginx (restart nginx)
+echo -e "\e[31m[Nginx] Restart ...\e[0m"
+/etc/init.d/nginx restart
